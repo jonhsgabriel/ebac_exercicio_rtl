09ffabd8f7b17e80fbb9903a7778d5d101db8166
João Gabriel da Motta Gomes <joaomotta@redejmu.local>
2024-06-21 15:30:18 -0300

    Resolução do exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    it('Deve renderizar o componente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('comentario-textarea'), {
+            target: {
+                value: ' 1° Comentário adicionado',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comentario-butao'));
+
+        fireEvent.change(screen.getByTestId('comentario-textarea'), {
+            target: {
+                value: '2° comentário adicionado',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comentario-butao'));
+
+        expect(screen.getAllByTestId('comentario-elemento')).toHaveLength(2);
     });
 });
